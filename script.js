b326212a2eab61c57e1bca99f549030c7ab3f7f8
Constantin <dontu.31@mail.ru>
2022-06-29 14:38:30 +0300

    localStorage

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ let popupClose = document.getElementById("popup__close");
 let signUpPassword = document.getElementById("signup__password");
 let confirmPassword = document.getElementById("confirm__password");
 let signupEmail = document.getElementById("signup__email");
+const errorBorder = "1px solid red";
+const rightBorder = "1px solid #1A1C22";
 submit.addEventListener("click", () => {
-  if (signUpPassword.value !== confirmPassword.value || signupEmail.value == false) {
+  if (signUpPassword.value !== confirmPassword.value || signupEmail.value == false || signUpPassword.value == false) {
     popup.style.opacity = "1";
+    ( signupEmail.value == false ) ? signupEmail.style.border = errorBorder:  signupEmail.style.border = rightBorder;
+    ( signUpPassword.value !== confirmPassword.value || signUpPassword.value == false) ? confirmPassword.style.border = errorBorder : confirmPassword.style.border = rightBorder;
+    if ( signUpPassword.value == false ) {
+      signUpPassword.style.border = errorBorder;
+    } else {
+      signUpPassword.style.border = rightBorder;
+    }
+  } else {
+    localStorage.setItem('email', signupEmail.value);
+    localStorage.setItem('password', signUpPassword.value);
+    popup.style.opacity = "0";
+    signupEmail.style.border = rightBorder;
+    signUpPassword.style.border = rightBorder;
+    confirmPassword.style.border = rightBorder;
   }
 })
 popupClose.addEventListener("click", () => {
